1fb10b2026daf2952b108586218fba48c6297181
rkhademul8 <rkhademul8.com>
2022-03-06 23:19:06 +0600

    data-load-done

---
+++ src/componets/Product/Product.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    console.log(props.product);
+    const {img,name,price,seller,stock}=props.product
+    return (
+        <div className='product-info'>
+          <div className="product-img">
+             <img src={img} alt="" />
+              </div>
+
+           <div className="product-details">
+                <h4>{name}</h4>
+                <p>{seller}</p>
+                <h5>{price}</h5>
+                <p>only {stock} left in stock</p>
+                <button>Add to cart</button>
+               </div>
+
+            
+        </div>
+    );
+};
+
+export default Product;
--- src/componets/Shop/Shop.js
+++ src/componets/Shop/Shop.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css'
 const Shop = () => {
+    const [products, setProducts]=useState([])
+
+    useEffect(()=>{
+        fetch('./products.JSON')
+        .then(res=>res.json())
+        .then(data=>setProducts(data))
+    },[])
     return (
         <div className='product-page'>
            <div className="product">
                <h2>Product Details</h2>
+               {
+                  products.map(product=><Product
+                  key={product.key}
+                  product={product}
+                  ></Product>) 
+                  
+               }
+               
                </div>
            <div className="cart">
                <h3>Order Summery</h3>
