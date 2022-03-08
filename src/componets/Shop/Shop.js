5df7b83d6936576c9e3de392b7ebeaaebac3d579
rkhademul8 <rkhademul8.com>
2022-03-08 12:15:47 +0600

    search_part_done

--- src/componets/Shop/Shop.js
+++ src/componets/Shop/Shop.js
@@ -8,7 +8,10 @@ const Shop = () => {
     useEffect(()=>{
         fetch('./products.JSON')
         .then(res=>res.json())
-        .then(data=>setProducts(data))
+        .then(data=>{
+            setProducts(data)
+            setDisplayProduct(data)
+        })
     },[])
 
     //  product button handle
@@ -19,12 +22,35 @@ const Shop = () => {
         const newCart=[...cart,product]
         setCart(newCart)
     }
+
+
+    // search handle
+    const [displayProduct, setDisplayProduct]=useState([])
+
+    const handleSearch=event=>{
+        // console.log(event.target.value);
+        const searchText=event.target.value
+        const matchedProduct=products.filter(product=>product.name.toLowerCase().includes(searchText.toLowerCase()))
+        // console.log(matchedProduct.length);
+        setDisplayProduct(matchedProduct)
+
+    }
     return (
+     <>
+
+        <div className="search-section">
+            
+            <input type="text"
+            placeholder='search your product'
+            onChange={handleSearch}
+            />
+            </div>
+
         <div className='product-page'>
            <div className="product">
                <h2>Product Details</h2>
                {
-                  products.map(product=><Product
+                  displayProduct.map(product=><Product
                   key={product.key}
                   product={product}
                   handleAddToCart={handleAddToCart}
@@ -37,6 +63,7 @@ const Shop = () => {
                <Cart cart={cart}></Cart>
                </div>
         </div>
+     </>
     );
 };
 
